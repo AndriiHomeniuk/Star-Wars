f7dbd0f9eb5cc795199fb7a69635715df2fd4177
ahomeniuk <andriy.homeniuk@gmail.com>
2020-05-19 11:27:18 +0300

    create planet template and reduce duplicated code

---
+++ game/messages.py
@@ -0,0 +1,23 @@
+def introduction():
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~Welcome to the world of Star Wars~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('~~~~~~~~~~~~~~~~~~~~Luke Skywalker has returned to his home planet of Tatooine~~~~~~~~~~~~~~~~~~~~')
+    print('in an attempt to rescue his friend Han Solo from the clutches of the vile gangster Jabba the Hutt.')
+    print('~~~~~~~~~~Little does Luke know that the GALACTIC EMPIRE has secretly begun construction~~~~~~~~~~')
+    print('~~~~~~~on a new armored space station even more powerful than the first dreaded Death Star.~~~~~~~')
+    print('~~~~~When completed, this ultimate weapon will spell certain doom for the small band of rebels~~~~')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~struggling to restore freedom to the galaxy...~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~You are on the planet Geonosis.~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('~~~~~~~~~~~~~~~In order to save the galaxy, you need to find the master of Yoda.~~~~~~~~~~~~~~~~~~')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~But nobody knows where he is.~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('~So you will have to fly all the systems and ask everyone who can know where the great master is.~')
+    print('~~~~~~~~~~And do not hesitate. There is very little time. You need to make it to May 4th.~~~~~~~~~')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~So, you have 20 days.~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+
+def final_msg():
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~May the force be with you.~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
--- game/planets.py
+++ game/planets.py
@@ -1,8 +1,32 @@
+import re
+
 FLY_COMMAND = ('Fly', 'fly')
 CHOICE_MSG = 'What do you choose? "Fly" or some planet? > '
 DELIMITER = '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 FLY_AROUND_PLANET_MSG = 'You\'ve flown around the planet. Unfortunately, there is almost no life, except for local primitive animals. You spent 1 day. Look further!'
 WASTING_MSG = 'Hey, Young padavan, do not waste your time! Do something.'
+PLANET_DICT = {
+	"Geonosis": 1,
+	"Felucia": 2,
+	"Mustafor": 3,
+	"Polis_Massa": 4,
+	"Alderaan": 5,
+	"Tatooine": 6,
+	"Kamino": 7,
+	"Utapau": 8,
+	"Keshyyk": 9,
+	"Corasant": 10,
+	"Nabu": 11,
+	"Chot": 12,
+	"Dagoba": 13,
+	"Yavin": 14,
+}
+
+
+def parse_system(message):
+	raw_data = re.search(r'systems: (?P<planets>.+). Or', message).group('planets')
+	planets = re.sub('or', ',', raw_data).split(',')
+	return [planet.strip() for planet in planets]
 
 
 def wasting_flying():
@@ -15,353 +39,117 @@ def flying_around_planet():
 	print(FLY_AROUND_PLANET_MSG)
 
 
-def geonosis(day_counter):
+def fly_to_planet(planet):
 	print(DELIMITER)
-	print("The planet has always been a droid factory. From here you can fly in 3 systems: Felucia, Kamino or Utapau. Or you can fly the planet itself and look here.")
+	print(f"You fly to {planet} and spend 1 day.")
+	return PLANET_DICT[planet]
+
+
+def planet_template(planet_msg, planet_index, day_counter):
+	print(DELIMITER)
+	print(planet_msg)
 	day_counter += 1
 	result_of_choice = str(input(CHOICE_MSG))
-	planet_index = 1
 	if result_of_choice in FLY_COMMAND:
 		flying_around_planet()
-	elif result_of_choice == "Felucia":
-		print(DELIMITER)
-		print("You fly to Felucia and spend 1 day.")
-		planet_index = 2
-	elif result_of_choice == "Kamino":
-		print(DELIMITER)
-		print("You fly to Kamino and spend 1 day.")
-		planet_index = 7
-	elif result_of_choice == "Utapau":
-		print(DELIMITER)
-		print("You fly to Utapau and spend 1 day.")
-		planet_index = 8
+	elif result_of_choice in parse_system(planet_msg):
+		planet_index = fly_to_planet(result_of_choice)
 	else:
 		wasting_flying()
 	return planet_index, day_counter
+
+
+def geonosis(day_counter):
+	planet_msg = "The planet has always been a droid factory. From here you can fly in 3 systems: Felucia, Kamino or Utapau. Or you can fly the planet itself and look here."
+	planet_index = 1
+	return planet_template(planet_msg, planet_index, day_counter)
 
 
 def felucia(day_counter):
-	print(DELIMITER)
-	print("The planet has jungle. From here you can fly in 3 systems: Geonosis, Mustafor or Tatooine. Or you can fly the planet itself and look here.")
-	day_counter += 1
-	result_of_choice = str(input(CHOICE_MSG))
+	planet_msg = "The planet has jungle. From here you can fly in 3 systems: Geonosis, Mustafor or Tatooine. Or you can fly the planet itself and look here."
 	planet_index = 2
-	if result_of_choice in FLY_COMMAND:
-		flying_around_planet()
-	elif result_of_choice == "Geonosis":
-		print(DELIMITER)
-		print("You fly to Geonosis and spend 1 day.")
-		planet_index = 1 
-	elif result_of_choice == "Mustafor":
-		print(DELIMITER)
-		print("You fly to Mustafor and spend 1 day.")
-		planet_index = 3
-	elif result_of_choice == "Tatooine":
-		print(DELIMITER)
-		print("You fly to Tatooine and spend 1 day.")
-		planet_index = 6
-	else:
-		wasting_flying()
-	return planet_index, day_counter
+	return planet_template(planet_msg, planet_index, day_counter)
 
 
 def mustafor(day_counter):
-	print(DELIMITER)
-	print("From here you can fly in 2 systems: Felucia, Polis-Massa. Or you can fly the planet itself and look here.")
-	day_counter += 1
-	result_of_choice = str(input(CHOICE_MSG))
+	planet_msg = "From here you can fly in 2 systems: Felucia, Polis-Massa. Or you can fly the planet itself and look here."
 	planet_index = 3
-	if result_of_choice in FLY_COMMAND:
-		flying_around_planet()
-	elif result_of_choice == "Felucia":
-		print(DELIMITER)
-		print("You fly to Felucia and spend 1 day.")
-		planet_index = 2
-	elif result_of_choice == "Polis-Massa":
-		print(DELIMITER)
-		print("You fly to Polis-Massa and spend 1 day.")
-		planet_index = 4
-	else:
-		wasting_flying()
-	return planet_index, day_counter
+	return planet_template(planet_msg, planet_index, day_counter)
 
 
 def polis_massa(day_counter):
-	print(DELIMITER)
-	print("From here you can fly in 4 systems: Yavin, Mustafor, Tatooine, Alderaan. Or you can fly the planet itself and look here.")
-	day_counter += 1
-	result_of_choice = str(input(CHOICE_MSG))
+	planet_msg = "From here you can fly in 4 systems: Yavin, Mustafor, Tatooine, Alderaan. Or you can fly the planet itself and look here."
 	planet_index = 4
-	if result_of_choice in FLY_COMMAND:
-		flying_around_planet()
-	elif result_of_choice == "Yavin":
-		print(DELIMITER)
-		print("You fly to Yavin and spend 1 day.")
-		planet_index = 14 
-	elif result_of_choice == "Mustafor":
-		print(DELIMITER)
-		print("You fly to Mustafor and spend 1 day.")
-		planet_index = 3
-	elif result_of_choice == "Tatooine":
-		print(DELIMITER)
-		print("You fly to Tatooine and spend 1 day.")
-		planet_index = 6
-	elif result_of_choice == "Alderaan":
-		print(DELIMITER)
-		print("You fly to Alderaan and spend 1 day.")
-		planet_index = 5
-	else:
-		wasting_flying()
-	return planet_index, day_counter
+	return planet_template(planet_msg, planet_index, day_counter)
 
 
 def alderaan(day_counter):
-	print(DELIMITER)
-	print("Alderaan has the capital - Aldera. There you can find something interesting. From here you can fly in 3 systems: Polis-Massa, Nabu, Chot. Or you can fly the planet itself and look here.")
-	day_counter += 1
-	result_of_choice = str(input(CHOICE_MSG))
+	planet_msg = "Alderaan has the capital - Aldera. There you can find something interesting. From here you can fly in 3 systems: Polis-Massa, Nabu, Chot. Or you can fly the planet itself and look here."
 	planet_index = 5
-	if result_of_choice in FLY_COMMAND:
-		flying_around_planet()
-	elif result_of_choice == "Polis-Massa":
-		print(DELIMITER)
-		print("You fly to Polis-Massa and spend 1 day.")
-		planet_index = 4
-	elif result_of_choice == "Nabu":
-		print(DELIMITER)
-		print("You fly to Nabu and spend 1 day.")
-		planet_index = 11
-	elif result_of_choice == "Chot":
-		print(DELIMITER)
-		print("You fly to Chot and spend 1 day.")
-		planet_index = 12
-	else:
-		wasting_flying()
-	return planet_index, day_counter
+	return planet_template(planet_msg, planet_index, day_counter)
 
 
 def tatooine(day_counter):
-	print(DELIMITER)
-	print("The planet has only desert and nothing else. From here you can fly in 4 systems: Felucia, Polis-Massa, Nabu, Kamino. Or you can fly the planet itself and look here.")
-	day_counter += 1
-	result_of_choice = str(input(CHOICE_MSG))
+	planet_msg = "The planet has only desert and nothing else. From here you can fly in 4 systems: Felucia, Polis-Massa, Nabu, Kamino. Or you can fly the planet itself and look here."
 	planet_index = 6
-	if result_of_choice in FLY_COMMAND:
-		flying_around_planet()
-	elif result_of_choice == "Felucia":
-		print(DELIMITER)
-		print("You fly to Felucia and spend 1 day.")
-		planet_index = 2
-	elif result_of_choice == "Polis-Massa":
-		print(DELIMITER)
-		print("You fly to Polis-Massa and spend 1 day.")
-		planet_index = 4
-	elif result_of_choice == "Nabu":
-		print(DELIMITER)
-		print("You fly to Nabu and spend 1 day.")
-		planet_index = 11
-	elif result_of_choice == "Kamino":
-		print(DELIMITER)
-		print("You fly to Kamino and spend 1 day.")
-		planet_index = 7
-	else:
-		wasting_flying()
-	return planet_index, day_counter
+	return planet_template(planet_msg, planet_index, day_counter)
 
 
 def kamino(day_counter):
-	print(DELIMITER)
-	print("From here you can fly in 5 systems: Geonosis, Tatooine, Utapau, Nabu, Corasant. Or you can fly the planet itself and look here.")
-	day_counter += 1
-	result_of_choice = str(input(CHOICE_MSG))
+	planet_msg = "From here you can fly in 5 systems: Geonosis, Tatooine, Utapau, Nabu, Corasant. Or you can fly the planet itself and look here."
 	planet_index = 7
-	if result_of_choice in FLY_COMMAND:
-		flying_around_planet()
-	elif result_of_choice == "Geonosis":
-		print(DELIMITER)
-		print("You fly to Geonosis and spend 1 day.")
-		planet_index = 1 
-	elif result_of_choice == "Utapau":
-		print(DELIMITER)
-		print("You fly to Utapau and spend 1 day.")
-		planet_index = 8
-	elif result_of_choice == "Tatooine":
-		print(DELIMITER)
-		print("You fly to Tatooine and spend 1 day.")
-		planet_index = 6
-	elif result_of_choice == "Nabu":
-		print(DELIMITER)
-		print("You fly to Nabu and spend 1 day.")
-		planet_index = 11
-	elif result_of_choice == "Corasant":
-		print(DELIMITER)
-		print("You fly to Corasant and spend 1 day.")
-		planet_index = 10
-	else:
-		wasting_flying()
-	return planet_index, day_counter
+	return planet_template(planet_msg, planet_index, day_counter)
 
 
 def utapau(day_counter):
-	print(DELIMITER)
-	print("From here you can fly in 3 systems: Geonosis, Kamino, Keshyyk. Or you can fly the planet itself and look here.")
-	day_counter += 1
-	result_of_choice = str(input(CHOICE_MSG))
+	planet_msg = "From here you can fly in 3 systems: Geonosis, Kamino, Keshyyk. Or you can fly the planet itself and look here."
 	planet_index = 8
-	if result_of_choice in FLY_COMMAND:
-		flying_around_planet()
-	elif result_of_choice == "Geonosis":
-		print(DELIMITER)
-		print("You fly to Geonosis and spend 1 day.")
-		planet_index = 1 
-	elif result_of_choice == "Kamino":
-		print(DELIMITER)
-		print("You fly to Kamino and spend 1 day.")
-		planet_index = 7
-	elif result_of_choice == "Keshyyk":
-		print(DELIMITER)
-		print("You fly to Keshyyk and spend 1 day.")
-		planet_index = 9
-	else:
-		wasting_flying()
-	return planet_index, day_counter
+	return planet_template(planet_msg, planet_index, day_counter)
 
 
 def keshyyk(day_counter):
-	print(DELIMITER)
-	print("The planet has a jungle, where the wookiees live. The capital of planet is Rwookrrorro. From here you can fly in 2 systems: Utapau, Corasant. Or you can fly the planet itself and look here.")
-	day_counter += 1
-	result_of_choice = str(input(CHOICE_MSG))
+	planet_msg = "The planet has a jungle, where the wookiees live. The capital of planet is Rwookrrorro. From here you can fly in 2 systems: Utapau, Corasant. Or you can fly the planet itself and look here."
 	planet_index = 9
-	if result_of_choice in FLY_COMMAND:
-		flying_around_planet()
-	elif result_of_choice == "Utapau":
-		print(DELIMITER)
-		print("You fly to Utapau and spend 1 day.")
-		planet_index = 8
-	elif result_of_choice == "Corasant":
-		print(DELIMITER)
-		print("You fly to Corasant and spend 1 day.")
-		planet_index = 10
-	else:
-		wasting_flying()
-	return planet_index, day_counter
+	return planet_template(planet_msg, planet_index, day_counter)
 
 
 def corasant(day_counter):
-	print(DELIMITER)
-	print("This giant planet-city has a 10 billion people. Here we can see Senat and Temple of the Jedi. From here you can fly in 3 systems: Kamino, Keshyyk, Dagoba. Or you can fly the planet itself and look here.")
-	day_counter += 1
-	result_of_choice = str(input(CHOICE_MSG))
+	planet_msg = "This giant planet-city has a 10 billion people. Here we can see Senat and Temple of the Jedi. From here you can fly in 3 systems: Kamino, Keshyyk, Dagoba. Or you can fly the planet itself and look here."
 	planet_index = 10
-	if result_of_choice in FLY_COMMAND:
-		flying_around_planet()
-	elif result_of_choice == "Kamino":
-		print(DELIMITER)
-		print("You fly to Kamino and spend 1 day.")
-		planet_index = 7
-	elif result_of_choice == "Keshyyk":
-		print(DELIMITER)
-		print("You fly to Keshyyk and spend 1 day.")
-		planet_index = 9
-	elif result_of_choice == "Dagoba":
-		print(DELIMITER)
-		print("You fly to Dagoba and spend 1 day.")
-		planet_index = 13
-	else:
-		wasting_flying()
-	return planet_index, day_counter
+	return planet_template(planet_msg, planet_index, day_counter)
 
 
 def nabu(day_counter):
-	print(DELIMITER)
-	print("More than 85 percentages of the surface of the planet is covered with waterFrom here you can fly in 5 systems: Chot, Dagoba, Kamino, Tatooine, Alderaan. Or you can fly the planet itself and look here.")
-	day_counter += 1
-	result_of_choice = str(input(CHOICE_MSG))
+	planet_msg = ("More than 85 percentages of the surface of the planet is covered with waterFrom here you can fly in 5 systems: Chot, Dagoba, Kamino, Tatooine, Alderaan. Or you can fly the planet itself and look here.")
 	planet_index = 11
-	if result_of_choice in FLY_COMMAND:
-		flying_around_planet()
-	elif result_of_choice == "Chot":
-		print(DELIMITER)
-		print("You fly to Chot and spend 1 day.")
-		planet_index = 12
-	elif result_of_choice == "Dagoba":
-		print(DELIMITER)
-		print("You fly to Dagoba and spend 1 day.")
-		planet_index = 13
-	elif result_of_choice == "Tatooine":
-		print(DELIMITER)
-		print("You fly to Tatooine and spend 1 day.")
-		planet_index = 6
-	elif result_of_choice == "Kamino":
-		print(DELIMITER)
-		print("You fly to Kamino and spend 1 day.")
-		planet_index = 7
-	elif result_of_choice == "Alderaan":
-		print(DELIMITER)
-		print("You fly to Alderaan and spend 1 day.")
-		planet_index = 5
-	else:
-		wasting_flying()
-	return planet_index, day_counter
+	return planet_template(planet_msg, planet_index, day_counter)
 
 
 def chot(day_counter):
-	print(DELIMITER)
-	print("From here you can fly in 2 systems: Nabu, Alderaan. Or you can fly the planet itself and look here.")
-	day_counter += 1
-	result_of_choice = str(input(CHOICE_MSG))
+	planet_msg = "From here you can fly in 2 systems: Nabu, Alderaan. Or you can fly the planet itself and look here."
 	planet_index = 12
-	if result_of_choice in FLY_COMMAND:
-		flying_around_planet()
-	elif result_of_choice == "Alderaan":
-		print(DELIMITER)
-		print("You fly to Alderaan and spend 1 day.")
-		planet_index = 5
-	elif result_of_choice == "Nabu":
-		print(DELIMITER)
-		print("You fly to Nabu and spend 1 day.")
-		planet_index = 11
-	else:
-		wasting_flying()
-	return planet_index, day_counter
+	return planet_template(planet_msg, planet_index, day_counter)
 
 
 def dagoba(day_counter):
-	print(DELIMITER)
-	print("From here you can fly in 2 systems: Nabu, Corasant. Or you can fly the planet itself and look here.")
-	day_counter += 1
-	result_of_choice = str(input(CHOICE_MSG))
+	planet_msg = "From here you can fly in 2 systems: Nabu, Corasant. Or you can fly the planet itself and look here."
 	planet_index = 13
-	if result_of_choice in FLY_COMMAND:
-		flying_around_planet()
-	elif result_of_choice == "Nabu":
-		print(DELIMITER)
-		print("You fly to Nabu and spend 1 day.")
-		planet_index = 11
-	elif result_of_choice == "Corasant":
-		print(DELIMITER)
-		print("You fly to Corasant and spend 1 day.")
-		planet_index = 10
-	else:
-		wasting_flying()
-	return planet_index, day_counter
+	return planet_template(planet_msg, planet_index, day_counter)
 
 
 def yavin(day_counter):
-	print(DELIMITER)
-	print("The planet has a lot of deciduous forests. From here you can fly in 1 systems: Polis-Massa. Or you can fly the planet itself and look here.")
+	planet_msg = "The planet has a lot of deciduous forests. From here you can fly in 1 systems: Polis-Massa. Or you can fly the planet itself and look here."
 	day_counter += 1
 	result_of_choice = str(input(CHOICE_MSG))
 	planet_index = 14
+	print(DELIMITER)
+	print(planet_msg)
 	if result_of_choice in FLY_COMMAND:
 		print(DELIMITER)
 		winn()
-		planet_index = -1  # winn()
-	elif result_of_choice == "Polis-Massa":
-		print(DELIMITER)
-		print("You fly to Polis-Massa and spend 1 day.")
-		planet_index = 4
+		planet_index = -1
+	elif result_of_choice in parse_system(planet_msg):
+		planet_index = fly_to_planet(result_of_choice)
 	else:
 		wasting_flying()
 	return planet_index, day_counter
